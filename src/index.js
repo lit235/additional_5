63607b8e2dad7b03e817005f4297ca2e5e1534cd
lit235 <31992702+lit235@users.noreply.github.com>
2017-09-30 20:59:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,23 +16,30 @@ module.exports = function check(str, bracketsConfig) {
 
         if (buff){
             if (str[i]==buff[0]){
-                if(str.length<i+2){
-                    return false;
+                if(buff[0]==buff[1] && i>0 && str[i-1]==buff[0]){
+                    i--;
+                    str = str.substr(0, i) + str.substr(i+2);
                 }
-                i++;
+                else {
+                    if(str.length<i+2){
+                    return false;
+                    }
+                    i++;
+                }
+
             }
             else{
                 if(i>0){
-                   if(buff[0]==str[i-1]){
-                       if(1<=i) {
-                           i--;
-                           str = str.substr(0, i) + str.substr(i+2);
+                    if(buff[0]==str[i-1]){
+                        if(1<=i) {
+                            i--;
+                            str = str.substr(0, i) + str.substr(i+2);
 
-                       }
-                       else {
-                           str="";
-                       }
-                   }else return false;
+                        }
+                        else {
+                            str="";
+                        }
+                    }else return false;
                 }
                 else return false;
             }
